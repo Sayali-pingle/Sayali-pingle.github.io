55b87b94136ededa59c63ddd63b5990ac94154c4
sayali-pingle <72289957+sayali-pingle@users.noreply.github.com>
2025-02-19 03:17:32 +0000

    Deploying to gh-pages from @ sayali-pingle/sayali-pingle.github.io@b83301497a6812f5a68c1d85b74fe793ea824f98 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,9 +421,9 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
+            },},{id: "projects-singapore-traffic-analytics",
+          title: 'Singapore Traffic Analytics',
+          description: "Creating Large HuggingFace Dataset and detecting traffic patterns using camera data",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
