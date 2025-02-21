66d161db2f484c45a9ecd82cee26250568937aa4
sayali-pingle <72289957+sayali-pingle@users.noreply.github.com>
2025-02-21 00:38:18 +0000

    Deploying to gh-pages from @ sayali-pingle/sayali-pingle.github.io@d96806d7c24ca3a2369402bc2b3b153e22148469 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -396,9 +396,9 @@ ninja.data = [{
           description: "R shiny dashboard for exploring and learning about your favorite artists.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-peak-planner",
+          title: 'Peak Planner',
+          description: "LLM-based scheduler integrating Apple Health &amp; Calendar data to optimize daily routine and automate task priority",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
