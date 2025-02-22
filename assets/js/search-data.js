f4e45cf75fe8b854e9bab76bb5cd2a9ce96f4bb0
sayali-pingle <72289957+sayali-pingle@users.noreply.github.com>
2025-02-22 00:15:23 +0000

    Deploying to gh-pages from @ sayali-pingle/sayali-pingle.github.io@63645447f6acc9b83637ae831d105d4671be7f13 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -406,9 +406,9 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-algorithmic-trading",
+          title: 'Algorithmic Trading',
+          description: "Adaptive Pairs Trading with Bayesian MCMC Sampling",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
